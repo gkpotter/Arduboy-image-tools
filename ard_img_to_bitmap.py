673614a8bfa529d41f82b2fb63fc1ad397bf1bd6
gkpotter <gkp@bu.edu>
2020-09-16 22:14:44 -0400

    Added ability to convert sprite sheet to bitmaps.

--- ard_img_to_bitmap.py
+++ ard_img_to_bitmap.py
@@ -8,6 +8,8 @@ def main():
 	parser.add_argument('-input_file', required = True)
 	parser.add_argument('-output_file', default = './bitmaps.h')
 
+	parser.add_argument('-frame_width', type=int)
+
 	args = parser.parse_args()
 
 	name = args.input_file.split('/')[-1].split('.')[0]
@@ -15,21 +17,31 @@ def main():
 	im = Image.open(args.input_file).convert('L')
 	w, h = im.size
 
+	if args.frame_width == None:
+		args.frame_width = w
+	elif w% args.frame_width != 0:
+		print('Uneven number of frames.')
+		return
+
+	is_array = w//args.frame_width > 1
+
 	if h%8 != 0:
 		print('Height not dividible by 8. By default, the Arduboy reads one byte at a time and draws eight pixels vertically.')
 		return
 
 	pix = im.load()
 	
-	output = 'const unsigned char PROGMEM ' + name +'[] = {\n'
+	output = 'const unsigned char PROGMEM ' + name +'[]'+('[]' if is_array else '')+' = {\n'
 
-	for y_off in range(0,h,8):
-		for x in range(w):
-			block = 0
-			for y in range(y_off+7,y_off-1,-1):
-				block += 2**(y-y_off)*(pix[x,y]>args.threshold)
-			output+= str(hex(block)) + ','
-		output +='\n'
+	for frame in range(w//args.frame_width):
+		output+=('{' if is_array else '')
+		for y_off in range(0,h,8):
+			for x in range(frame*args.frame_width, (frame+1)*args.frame_width):
+				block = 0
+				for y in range(y_off+7,y_off-1,-1):
+					block += 2**(y-y_off)*(pix[x,y]>args.threshold)
+				output+= str(hex(block)) + ','
+			output += ('},\n' if is_array and y_off == h - 8 else '\n')
 	output+='};\n'
 
 	output_file = open(args.output_file, 'a')
